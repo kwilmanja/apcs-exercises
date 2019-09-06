2c930a7885ab4c8d73b883a28fbfb6efb459b129
Joe Kwilman <winnie.joe123@gmail.com>
2019-09-06 12:35:27 -0600

    add for loop and array into YourAgeInSeconds

--- 16chapter/YourAgeInSeconds.java
+++ 16chapter/YourAgeInSeconds.java
@@ -12,69 +12,18 @@ public class YourAgeInSeconds
 		System.out.println("Days: ");
 		int days = scan.nextInt();
 
-		int month1 = 31;
-		int month2 = 28;
-		int month3 = 31;
-		int month4 = 30;
-		int month5 = 31;
-		int month6 = 30;
-		int month7 = 31;
-		int month8 = 31;
-		int month9 = 30;
-		int month10 = 31;
-		int month11 = 30;
 
-		if (months <= 0)
-		{
-			monthDays = month1
-		}
-		if (months < 1)
-		{
-			monthDays = month1
-		}
-		if (months < 3)
-		{
-			monthDays = month1
-		}
-		if (months < 4)
-		{
-			monthDays = month1
-		}
-		if (months < 5)
-		{
-			monthDays = month1
-		}
-		if (months < 6)
-		{
-			monthDays = month1
-		}
-		if (months < 7)
-		{
-			monthDays = month1
-		}
-		if (months < 8)
-		{
-			monthDays = month1
-		}
-		if (months < 9)
-		{
-			monthDays = month1
-		}
-		if (months < 10)
-		{
-			monthDays = month1
-		}
-		if (months < 11)
-		{
-			monthDays = month1
-		}
-		if (months < 12)
-		{
-			monthDays = month1
+		int monthDays = 0;
+		int[] monthData = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+		//				   0   1   2   3   4   5   6   7   8   9   10 
+
+		int count;
+		for ( count = months; count > 0; count--) {
+			monthDays = monthDays + monthData[count - 1];
 		}
 
+		int totalDays = days + years * 365 + monthDays;
 
-		int totalDays = days + years * 365 + monthDays
 		System.out.println("Total Seconds: " + (totalDays * 24 * 60 * 60));
 	}
 
