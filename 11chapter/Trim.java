e51536f332b71c8e012a5f7011b077cabf31aa87
Joseph Allen Kwilman <kwilmanja@s.dcsdk12.org>
2020-01-08 08:33:22 -0700

    update chapter 11

--- 11chapter/Trim.java
+++ 11chapter/Trim.java
@@ -2,7 +2,7 @@ public class Trim
 {
 	public static void main (String[] args)
 	{
-		String first = new String("       my name is Caden		 ");
+		String first = new String("       my name is Cayden		 ");
 		String second = first.trim();
 		System.out.println(second);
 	}
